e280ec87573b6e35326a7a8b832702bf25741ad6
luho <luca.pasquarelli.villa@gmail.com>
2025-03-01 15:17:24 +0100

    expand data model

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 
 from fastapi import FastAPI
 from app.models import Venue
@@ -10,24 +10,33 @@ app = FastAPI()
 supabase = create_supabase_client()
 app.get("/")(lambda: {"Welcome": "To Florence"})
 
-# Retrieve a venue
-@app.get("/venue")
-def get_venue(venue_id: Union[str, None] = None):
+# Retrieve a single venue
+@app.get("/venue/{venue_id}", response_model=Venue)
+def get_venue(venue_id: str):
     try:
-        if venue_id:
-            venue = supabase.from_("florence_open_data")\
-                .select("id", "venue_name")\
-                .eq("id", venue_id)\
-                .execute()
+        venue = supabase.from_("florence_open_data")\
+            .select("id, venue_name, venue_type, venue_add_one, venue_web, latitude, longitude, wday_opening, wday_closing, wkd_opening, wkd_closing, variations, venue_capacity, instagram, quartiere")\
+            .eq("id", venue_id)\
+            .execute()
 
-            if venue:
-                return venue
-        else:
-            venues = supabase.from_("florence_open_data")\
-                .select("id", "venue_name")\
-                .execute()
-            if venues:
-                return venues
+        if venue and venue.data:
+            return venue.data[0]
+        return {"message": "venue not found"}
     except Exception as e:
         print(f"Error: {e}")
         return {"message": "venue not found"}
+
+# Retrieve all venues
+@app.get("/venues", response_model=List[Venue])
+def get_venues():
+    try:
+        venues = supabase.from_("florence_open_data")\
+            .select("id, venue_name, venue_type, venue_add_one, venue_web, latitude, longitude, wday_opening, wday_closing, wkd_opening, wkd_closing, variations, venue_capacity, instagram, quartiere")\
+            .execute()
+        
+        if venues and venues.data:
+            return venues.data
+        return []
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
--- app/models.py
+++ app/models.py
@@ -2,5 +2,18 @@ from pydantic import BaseModel
 
 class Venue(BaseModel):
     id: int
-    name: str
-    
+    venue_name: str
+    venue_type: str | None
+    venue_add_one: str | None
+    venue_web: str | None
+    latitude: float | None
+    longitude: float | None
+    wday_opening: str | None
+    wday_closing: str | None
+    wkd_opening: str | None
+    wkd_closing: str | None
+    variations: str | None
+    venue_capacity: str | None
+    instagram: str | None
+    quartiere: str | None
+  
